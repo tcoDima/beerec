6e02d8ad787db8b819483fbc27bb512e7a78f926
tcoDima <dvyhnanchuk@gmail.com>
2024-12-18 23:16:44 +0100

    Removing compiler problems + cleaning classNames and styling

--- src/components/Playerlist.tsx
+++ src/components/Playerlist.tsx
@@ -9,6 +9,7 @@ import {
   updateDoc,
 } from "firebase/firestore";
 import "./Playerlist.css";
+import trashIcon from "../assets/trash-icon.svg";
 
 const Playerlist = () => {
   const [players, setPlayers] = useState([]);
@@ -29,20 +30,20 @@ const Playerlist = () => {
   }, []);
 
   // Delete Player Function
-  const deletePlayer = async (id) => {
+  const deletePlayer = async (id: string) => {
     const playerDoc = doc(db, "players", id);
     await deleteDoc(playerDoc);
   };
 
   // Update Wins Functions
-  const plusWin = async (id, wins, losses) => {
+  const plusWin = async (id: string, wins: number, losses: number) => {
     const playerDoc = doc(db, "players", id);
     const updatedWins = wins + 1;
     const newScore = updatedWins / (losses || 1);
     const newFields = { wins: updatedWins, score: newScore };
     await updateDoc(playerDoc, newFields);
   };
-  const minusWin = async (id, wins, losses) => {
+  const minusWin = async (id: string, wins: number, losses: number) => {
     const playerDoc = doc(db, "players", id);
     if (wins < 1) return;
     const updatedWins = wins - 1;
@@ -52,14 +53,14 @@ const Playerlist = () => {
   };
 
   // Update Losses Functions
-  const plusLose = async (id, wins, losses) => {
+  const plusLose = async (id: string, wins: number, losses: number) => {
     const playerDoc = doc(db, "players", id);
     const updatedLosses = losses + 1;
     const newScore = wins / (updatedLosses || 1);
     const newFields = { losses: updatedLosses, score: newScore };
     await updateDoc(playerDoc, newFields);
   };
-  const minusLose = async (id, wins, losses) => {
+  const minusLose = async (id: string, wins: number, losses: number) => {
     const playerDoc = doc(db, "players", id);
     if (losses < 1) return;
     const updatedLosses = losses - 1;
@@ -69,7 +70,7 @@ const Playerlist = () => {
   };
 
   // Format score to maxim of 3 decimal points
-  const formatScore = (score) => {
+  const formatScore = (score: number) => {
     if (score % 1 === 0) {
       return score;
     }
@@ -182,15 +183,7 @@ const Playerlist = () => {
                     deletePlayer(player.id);
                   }}
                 >
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 448 512"
-                    width="25"
-                    height="25"
-                    fill="#6A6A6A"
-                  >
-                    <path d="M135.2 17.7L128 32 32 32C14.3 32 0 46.3 0 64S14.3 96 32 96l384 0c17.7 0 32-14.3 32-32s-14.3-32-32-32l-96 0-7.2-14.3C307.4 6.8 296.3 0 284.2 0L163.8 0c-12.1 0-23.2 6.8-28.6 17.7zM416 128L32 128 53.2 467c1.6 25.3 22.6 45 47.9 45l245.8 0c25.3 0 46.3-19.7 47.9-45L416 128z" />
-                  </svg>
+                  <img src={trashIcon} alt="Delete Player" />
                 </button>
               </div>
             </div>
